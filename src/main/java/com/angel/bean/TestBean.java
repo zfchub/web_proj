4e898437944c4fb6f5d931ff27e1d1fc56a3294f
Fangchao Zhou <zhoufangchao@lianfan.com>
2018-09-17 13:45:02 +0800

    some jsp

---
+++ src/main/java/com/angel/bean/TestBean.java
@@ -0,0 +1,16 @@
+package com.angel.bean;
+
+/**
+ * user: zhoufangchao
+ * date: 2018/9/17
+ */
+public class TestBean {
+    private String message = "菜鸟教程";
+
+    public String getMessage() {
+        return(message);
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
